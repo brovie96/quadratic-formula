21e6f8c6318e5ef5f10ac57ef60c3114ec7b0f76
brovie96 <brovie96@Seths-MacBook-Pro.local>
2013-10-15 15:44:04 -0700

    Version 1.2
    Better handling of imaginary numbers.

--- QuadraticFormula.java
+++ QuadraticFormula.java
@@ -1,6 +1,7 @@
 /* Author: Seth Riedel
  * Purpose: Do the quadratic formula really fast
- * Version 1.1 (with input, partially handles imaginary answers)
+ * Version 1.2 (better handles imaginary answers)
+ * Get newest version at http://github.com/brovie96/quadratic-formula
  */
 import java.util.Scanner; //import scanner
 public class QuadraticFormula
@@ -13,8 +14,11 @@ public class QuadraticFormula
       double b;
       double c;
       double discrim;
+      double discrimAginary;
       double firstAnswer;
       double secondAnswer;
+      double firstAnswerI;
+      double secondAnswerI;
       //get input
       System.out.print("Enter a value for a: ");
       a = input.nextDouble();
@@ -26,14 +30,17 @@ public class QuadraticFormula
       discrim = Math.sqrt(Math.pow(b,2) - 4*a*c);
       firstAnswer = (-b + discrim)/(2*a);
       secondAnswer = (-b - discrim)/(2*a);
+      discrimAginary = Math.sqrt(-(Math.pow(b,2) - 4*a*c));
+      firstAnswerI = (-b + discrimAginary)/(4*a*c);
+      secondAnswerI = (-b - discrimAginary)/(4*a*c);
       //print result
       if (firstAnswer == secondAnswer) System.out.printf("The answer is %f.%n",firstAnswer);
       else if (Double.isNaN(firstAnswer))
       {
-         if (Double.isNaN(secondAnswer)) System.out.println("There are no real answers.");
-         else System.out.printf("The only real answer is %f.%n",secondAnswer);
+         if (Double.isNaN(secondAnswer)) System.out.printf("The answers are %fi and %fi.%n",firstAnswerI,secondAnswerI);
+         else System.out.printf("The answers are %fi and %f.%n",firstAnswerI,secondAnswer);
       } //end if
-      else if (Double.isNaN(secondAnswer)) System.out.printf("The only real answer is %f.%n",firstAnswer);
+      else if (Double.isNaN(secondAnswer)) System.out.printf("The answers are %f and %fi.%n",firstAnswer,secondAnswerI);
       else System.out.printf("The answers are %f and %f.%n",firstAnswer,secondAnswer);
    } //end main method
 } //end class

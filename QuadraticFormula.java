4c4d2ff8051f8e07c9ce3e8541de93ae41ce565a
brovie96 <brovie96@Seths-MacBook-Pro.local>
2013-10-15 14:39:11 -0700

    Version 1.0
    Adds input and an if loop for equations with a single answer.

--- QuadraticFormula.java
+++ QuadraticFormula.java
@@ -1,24 +1,33 @@
 /* Author: Seth Riedel
  * Purpose: Do the quadratic formula really fast
- * Version 0.5 (test with x^2 - 1 = 0; results should be 1 and -1)
+ * Version 1.0 (with input)
  */
+import java.util.Scanner; //import scanner
 public class QuadraticFormula
 {
    public static void main(String[] args)
    {
+      Scanner input = new Scanner(System.in); //create scanner
       //define variables
-      double a = 1;
-      double b = 0;
-      double c = -1;
+      double a;
+      double b;
+      double c;
       double discrim;
       double firstAnswer;
       double secondAnswer;
-      //get input (coming in version 1.0)
+      //get input
+      System.out.print("Enter a value for a: ");
+      a = input.nextDouble();
+      System.out.print("Enter a value for b: ");
+      b = input.nextDouble();
+      System.out.print("Enter a value for c: ");
+      c = input.nextDouble();
       //calculations
       discrim = Math.sqrt(Math.pow(b,2) - 4*a*c);
       firstAnswer = (-b + discrim)/(2*a);
       secondAnswer = (-b - discrim)/(2*a);
       //print result
-      System.out.printf("The answers are %f and %f.%n",firstAnswer,secondAnswer);
+      if (firstAnswer == secondAnswer) System.out.printf("The answer is %f.%n",firstAnswer);
+      else System.out.printf("The answers are %f and %f.%n",firstAnswer,secondAnswer);
    } //end main method
 } //end class

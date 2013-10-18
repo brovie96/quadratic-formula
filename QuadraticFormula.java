81c008eaa5cbd64d36aaaa8b21b45a13e552a5ac
brovie96 <brovie96@Seths-MacBook-Pro.local>
2013-10-17 17:03:39 -0700

    Version 1.7
    Prettifies UI, removes iffy plus-or-minus symbol

--- QuadraticFormula.java
+++ QuadraticFormula.java
@@ -1,6 +1,6 @@
 /* Author: Seth Riedel
  * Purpose: Do the quadratic formula really fast
- * Version 1.6 (adds better imaginary answer handing)
+ * Version 1.7 (prettifies UI)
  * Get newest version at http://github.com/brovie96/quadratic-formula
  */
 import java.util.Scanner; //import scanner
@@ -20,6 +20,8 @@ public class QuadraticFormula
       double answerIReal;
       double answerIMaginary;
       //get input
+      System.out.println("This program will solve a quadratic equation ax^2 + bx + c = 0 and give an answer to six decimal places.");
+      System.out.println();
       System.out.print("Enter a value for a: ");
       a = input.nextDouble();
       System.out.print("Enter a value for b: ");
@@ -34,10 +36,11 @@ public class QuadraticFormula
       answerIReal = -b/(2*a);
       answerIMaginary = discrimAginary/(2*a);
       //print result
+      System.out.println();
       if (firstAnswer == secondAnswer) System.out.printf("The answer is %f.%n",firstAnswer);
       else if (Double.isNaN(discrim))
       {
-         System.out.printf("The answers are %f ± %fi.%n",answerIReal,answerIMaginary);
+         System.out.printf("The answers are %f +/- %fi.%n",answerIReal,answerIMaginary);
       } //end if
       else System.out.printf("The answers are %f and %f.%n",firstAnswer,secondAnswer);
    } //end main method
